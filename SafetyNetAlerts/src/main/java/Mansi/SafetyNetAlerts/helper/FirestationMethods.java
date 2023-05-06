3bf8551ba5046d1313a724b5bafad345a916d3bf
Mansi1424 <mansi2001.edu@gmail.com>
2023-05-06 09:33:07 -0400

    Progress on first URL

---
+++ SafetyNetAlerts/src/main/java/Mansi/SafetyNetAlerts/helper/FirestationMethods.java
@@ -0,0 +1,38 @@
+package Mansi.SafetyNetAlerts.helper;
+
+import Mansi.SafetyNetAlerts.Model.Person;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+
+public class FirestationMethods {
+
+    public static int getAge(String Stringdob) throws ParseException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+        int age;
+        Date dateOfBirth = dateFormat.parse(Stringdob);
+        Calendar dob = Calendar.getInstance();
+        dob.setTime(dateOfBirth);
+
+        Calendar today = Calendar.getInstance();
+        age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+        if (today.get(Calendar.DAY_OF_YEAR) < dob.get(Calendar.DAY_OF_YEAR)) {
+            age--;
+        }
+        return age;
+
+
+    }
+
+    public static boolean containsPersonWithName(List<Person> list, String name) {
+        for (Person person : list) {
+            if (person.getFirstName().equals(name)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
